634426dda1cc193acf96c48bbdda9247dd9ebe70
felipecam <jfelipecamacho@hotmail.com>
2020-10-30 20:58:11 -0600

    Problema 2 commit

---
+++ src/proyectoprogra1/Problema2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoprogra1;
+
+/**
+ *
+ * @author fbriceno
+ */
+public class Problema2 {
+
+    public static void main(String[] args) {
+
+        //ejercicio 1
+        int num1 = 2;
+        int num2 = 3;;
+        int num3 = 5;;
+        int num4 = 7;;
+        int num5 = 10;;
+        int num6 = 3;;
+
+        int SumTotal = num1 + num2 + num3 + num4 + num5 + num6;
+        int promedio = SumTotal / 6;
+
+     System.out.println("Ejercicio 1, el promedio es: " + promedio);
+    }
+
+}
